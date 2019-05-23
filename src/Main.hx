9e45d706271281b53649d6eec4bd174a50f082ef
Sébastien Bénard <github@deepnight.net>
2019-05-24 00:31:17 +0200

    Fixed missing ssl.hddl in Windows redist

--- src/Main.hx
+++ src/Main.hx
@@ -12,6 +12,7 @@ class Main {
 		{ lib:null, f:"libhl.dll" },
 		{ lib:null, f:"msvcr120.dll" },
 		{ lib:null, f:"fmt.hdll" },
+		{ lib:null, f:"ssl.hdll" },
 
 		{ lib:"heaps", f:"OpenAL32.dll" },
 		{ lib:"heaps", f:"openal.hdll" },
